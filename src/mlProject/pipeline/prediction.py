300435e253c7d27ed85a3ed00753eca2e75683d5
Venura Pussella <137409412+Venura-94@users.noreply.github.com>
2024-04-18 14:30:18 +0530

    Update prediction.py

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-
+# Preprocessing block is under construction ,  cz model performances still researching to upgrade
 
 class PredictionPipeline:
     def __init__(self):
